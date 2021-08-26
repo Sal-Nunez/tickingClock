8a922b00c12584995a46d37adf0eebf38b21a2bf
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-08-25 19:57:09 -0700

    UPDATEINMSCLOCK

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ setInterval(function () {
     var currentHour = new Date().getHours()
     var currentMinute = new Date().getMinutes()
     var currentSecond = new Date().getSeconds()
+    var currentMSecon = new Date().getMilliseconds()
     var seconds = document.querySelector("#seconds");
     var minutes = document.getElementById("minutes");
     var hours = document.getElementById("hour");
-    var secondsDeg = currentSecond * 6 + 180;
-    var minutesDeg = currentMinute * 6 + 180
-    var hoursDeg = currentHour * 30 + 180
+    var secondsDeg = currentSecond * 6 + 180 + (currentMSecon*.00625) ;
+    var minutesDeg = currentMinute * 6 + 180 + (currentSecond*.1);
+    var hoursDeg = currentHour * 30 + 180 + (currentMinute*.5);
     rotate(seconds, secondsDeg)
     rotate(minutes, minutesDeg)
     rotate(hours, hoursDeg)
-}, 1000);
+}, 1);
 
 rotate = function (element, degrees) {
     element.style.transform = `rotate(${degrees}deg)`;
