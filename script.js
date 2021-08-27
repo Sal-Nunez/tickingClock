d5f8cdf14b581584fe9fa22ed1b8534cda2b2777
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-08-26 18:18:43 -0700

    msClock

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ setInterval(function () {
     var minutes = document.getElementById("minutes");
     var hours = document.getElementById("hour");
     var secondsDeg = currentSecond * 6 + 180 + (currentMSecon*.006) ;
-    var minutesDeg = currentMinute * 6 + 180 + (currentSecond*.1);
-    var hoursDeg = currentHour * 30 + 180 + (currentMinute*.5);
+    var minutesDeg = currentMinute * 6 + 180 + (currentSecond*.1) + (currentMSecon*.0001);
+    var hoursDeg = currentHour * 30 + 180 + (currentMinute*.5) + (currentSecond*.00833333) + (currentMSecon*.00000833);
     rotate(seconds, secondsDeg)
     rotate(minutes, minutesDeg)
     rotate(hours, hoursDeg)
